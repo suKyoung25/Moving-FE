5a7df33b605d1abcae483984db03dff57638c748
shimyubin <tla1161@naver.com>
2025-07-02 15:39:14 +0900

    feat: 버튼 컴포넌트 추가

---
+++ src/components/common/buttons/OutlinedButton.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+type OutlinedButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode;
+};
+
+export default function OutlinedButton({
+  children,
+  disabled,
+  ...props
+}: OutlinedButtonProps) {
+  return (
+    <button
+      type="button"
+      disabled={disabled}
+      className={`
+        w-full
+        px-4
+        rounded-2xl
+        text-16-semibold
+        transition
+        duration-200
+        bg-transparent
+        border-1 border-solid
+        border-primary-blue-300
+        text-primary-blue-300
+        hover:bg-primary-blue-50
+        disabled:bg-transparent
+        disabled:border-gray-100
+        disabled:text-gray-100
+        h-13.5
+        md:h-16 md:text-20-semibold
+        disabled:!cursor-not-allowed
+      `}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
