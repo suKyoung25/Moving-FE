52756590d105e71c31aa1fe05a5694ca78979e25
Hayoung Oh <scx200800@gmail.com>
2025-07-17 18:29:12 +0900

    feat: 기사님 상세 페이지 구현

--- src/app/(with-public)/mover-search/[id]/page.tsx
+++ src/app/(with-public)/mover-search/[id]/page.tsx
@@ -1,8 +1,9 @@
-// 기사님 찾기 상세
-export default function Page() {
-    return(
-        <div>
-            기사님 상세!
-        </div>
-    )
+import MoverDetail from "@/components/detail/MoverDetail";
+
+export default async function MoverDetailPage() {
+   return (
+      <div>
+         <MoverDetail />
+      </div>
+   );
 }
---
+++ src/components/detail/Line.tsx
@@ -0,0 +1,3 @@
+export default function Line() {
+   return <div className="border-line-100 border-b pb-5" />;
+}
