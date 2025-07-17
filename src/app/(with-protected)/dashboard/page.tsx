5bf6659cb0b5ad3d6c3e1d506fbac041b3956390
Hayoung Oh <scx200800@gmail.com>
2025-07-17 17:09:44 +0900

    feat: 기사 마이페이지 UI 구현

--- src/app/(with-protected)/dashboard/page.tsx
+++ src/app/(with-protected)/dashboard/page.tsx
@@ -1,11 +1,12 @@
 import PageTitle from "@/components/layout/PageTitle";
+import MyPage from "@/components/MyPage";
 
 // 기사님 마이페이지
 export default function Page() {
-    return(
-        <div>
-            <PageTitle title="마이페이지"/>
-            마이페이지
-        </div>
-    )
-} 
+   return (
+      <div>
+         <PageTitle title="마이페이지" />
+         <MyPage />
+      </div>
+   );
+}
