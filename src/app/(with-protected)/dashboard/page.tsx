4ad4ff218749d5d4444d45d89f0a1c295895fd2e
Hayoung Oh <scx200800@gmail.com>
2025-07-18 11:07:13 +0900

    feat: 기사 마이페이지 UI 구현

--- src/app/(with-protected)/dashboard/page.tsx
+++ src/app/(with-protected)/dashboard/page.tsx
@@ -1,12 +1,38 @@
+import LineDivider from "@/components/detail/LineDivider";
 import PageTitle from "@/components/layout/PageTitle";
-import MyPage from "@/components/MyPage";
+import MoverCard from "@/components/mover/MoverCard";
+import ReviewSection from "@/components/mover/ReviewSection";
+import EditButtons from "@/components/mover/EditButtons";
 
-// 기사님 마이페이지
-export default function Page() {
+const moverMock = {
+   id: "mover1",
+   profileImage: "http://www.zimssa.com/image-exmaple.png",
+   nickName: "류제천짱",
+   career: 2,
+   introduction: "고객님의 물품을 소중하고 안전하게 운송하여 드립니다.",
+   serviceType: ["SMALL", "HOME"],
+   serviceArea: ["서울", "경기"],
+   favoriteCount: 136,
+   estimateCount: 334,
+   averageReviewRating: 5.0,
+   reviewCount: 178,
+   isFavorite: true,
+};
+
+export default function MoverMyPage() {
    return (
-      <div>
+      <div className="min-h-screen">
          <PageTitle title="마이페이지" />
-         <MyPage />
+         <LineDivider className="my-6" />
+         <MoverCard mover={moverMock} />
+         <div className="flex gap-2 max-lg:mt-2.5 max-md:flex-col lg:hidden">
+            <EditButtons />
+         </div>
+         <LineDivider className="my-6 lg:mt-12 lg:mb-10" />
+         <ReviewSection
+            averageReviewRating={moverMock.averageReviewRating}
+            reviewCount={moverMock.reviewCount}
+         />
       </div>
    );
 }
