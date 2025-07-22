983f159885d1d92068acf31495679f5687d3122e
sky <blooming_garden_@naver.com>
2025-07-22 16:30:20 +0900

    중간저장

--- src/app/(with-protected)/profile/edit/page.tsx
+++ src/app/(with-protected)/profile/edit/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import ClientProfileTitle from "@/components/domain/profile/ClientProfileTitle";
-// import ClientProfileUpdateForm from "@/components/profile/ClientProfileUpdateForm";
+import ClientProfileUpdateForm from "@/components/domain/profile/ClientProfileUpdateForm";
 import { useAuth } from "@/context/AuthContext";
 
 export default function EditProfilePage() {
@@ -13,7 +13,7 @@ export default function EditProfilePage() {
          <div className="pt-4 pb-10 lg:pt-6">
             <div className="mx-auto max-w-82 lg:max-w-160">
                <ClientProfileTitle type="수정" />
-               {/* <ClientProfileUpdateForm /> */}
+               <ClientProfileUpdateForm />
             </div>
          </div>
       );
