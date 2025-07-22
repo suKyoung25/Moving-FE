890571e2fb5ff4316de7499bf7e8c4e727803085
Hayoung Oh <scx200800@gmail.com>
2025-07-22 14:52:42 +0900

    feat: 기사 마이페이지 링크 버튼 수정

--- src/components/domain/dashboard/EditButtons.tsx
+++ src/components/domain/dashboard/EditButtons.tsx
@@ -11,20 +11,24 @@ const btnClass = "flex w-full items-center justify-center gap-2";
 export default function EditButtons() {
    return (
       <>
-         <SolidButton className={btnClass}>
-            <Link href="/profile/edit">내 프로필 수정</Link>
-            <Image src={writingIcon} alt="프로필 수정" className="h-6 w-6" />
-         </SolidButton>
-         <OutlinedButton
-            className={`${btnClass} hover:!bg-bg-200 !border-gray-200 !text-gray-300`}
-         >
-            <Link href="/dashboard/edit-account">기본 정보 수정</Link>
-            <Image
-               src={writingIconGray}
-               alt="기본정보 수정"
-               className="h-6 w-6"
-            />
-         </OutlinedButton>
+         <Link href="/profile/edit" className="w-full">
+            <SolidButton className={btnClass}>
+               내 프로필 수정
+               <Image src={writingIcon} alt="프로필 수정" className="h-6 w-6" />
+            </SolidButton>
+         </Link>
+         <Link href="/dashboard/edit-account" className="w-full">
+            <OutlinedButton
+               className={`${btnClass} hover:!bg-bg-200 !border-gray-200 !text-gray-300`}
+            >
+               기본 정보 수정
+               <Image
+                  src={writingIconGray}
+                  alt="기본정보 수정"
+                  className="h-6 w-6"
+               />
+            </OutlinedButton>
+         </Link>
       </>
    );
 }
