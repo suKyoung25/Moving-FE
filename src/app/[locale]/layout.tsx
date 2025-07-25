6b1eb6ca4055174d66107546107d0d41551a2472
jbinyim <jbinyim12@naver.com>
2025-07-25 09:52:22 +0900

    refactor: 다국어 폴더 구조로 변경

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 
 export const metadata: Metadata = {
