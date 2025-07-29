2f7ad1b182a61680b2d2314c9b5f5c1fa8d0aeae
Hayoung Oh <scx200800@gmail.com>
2025-07-29 23:17:51 +0900

    refactor: 견적 요청 로딩 상태 ui 수정

--- src/app/[locale]/(with-protected)/request/page.tsx
+++ src/app/[locale]/(with-protected)/request/page.tsx
@@ -8,17 +8,20 @@ import { useState } from "react";
 
 export default function EstimateRequestPage() {
    const [currentStep, setCurrentStep] = useState(0);
+   const [isPending, setIsPending] = useState(true);
 
    return (
       <div className="bg-bg-200 min-h-screen">
          <Header>
-            <ProgressBar currentStep={currentStep} />
+            {!isPending && <ProgressBar currentStep={currentStep} />}
          </Header>
          <DefaultLayout>
             <div className="bg-bg-200 flex h-fit w-full flex-col pb-15 lg:px-65 lg:pb-54">
                <FormWizard
                   currentStep={currentStep}
                   setCurrentStep={setCurrentStep}
+                  isPending={isPending}
+                  setIsPending={setIsPending}
                />
             </div>
          </DefaultLayout>
