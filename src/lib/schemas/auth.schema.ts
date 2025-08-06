6cad5adb33ba019c65d49e19d4ab0c504486732f
suKyoung25 <s.kyoung5496@gmail.com>
2025-08-06 17:54:04 +0900

    fix: 비밀번호 특수문자 포함되게 수정

--- src/lib/schemas/auth.schema.ts
+++ src/lib/schemas/auth.schema.ts
@@ -10,8 +10,8 @@ export const emailSchema =
 export const passwordSchema = baseAuthSchema
    .min(8, "비밀번호를 8자리 이상 입력해 주세요.")
    .regex(
-      /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$/,
-      "문자와 숫자를 섞어 사용해 주세요.",
+      /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,16}$/,
+      "영문, 숫자, 특수문자를 조합해서 8~16자로 입력해 주세요.",
    );
 
 export const checkPasswordSchema = baseAuthSchema
