0a2acb0f4745ecf0e81975f8a54f981b36217792
shimyubin <tla1161@naver.com>
2025-08-11 11:21:33 +0900

    fix: 빌드오류 수정

--- src/lib/utils/formatAddress.util.ts
+++ src/lib/utils/formatAddress.util.ts
@@ -1,21 +1,16 @@
 // 주소 앞에 두 단어만 (영어일 때 뒤에 두 단어만)
 export default function formatAddress(address: string, locale: string) {
-   let parts = address.split(" ").filter(Boolean); // 공백 제거
+   const parts = address.split(" ").filter(Boolean); // 공백 제거
 
    if (locale === "en") {
       // "Republic of" 제거
-      const filteredParts: string[] = [];
-      for (let i = 0; i < parts.length; i++) {
-         // "Republic" 다음에 "of"가 오는 경우 스킵
-         if (
-            parts[i].toLowerCase() === "republic" &&
-            parts[i + 1]?.toLowerCase() === "of"
-         ) {
-            i++; // "of"도 같이 건너뛰기
-            continue;
-         }
-         filteredParts.push(parts[i]);
-      }
+      const filteredParts = parts.filter(
+         (_, idx) =>
+            !(
+               parts[idx].toLowerCase() === "republic" &&
+               parts[idx + 1]?.toLowerCase() === "of"
+            ),
+      );
 
       return filteredParts.length >= 2
          ? `${filteredParts[filteredParts.length - 2]} ${filteredParts[filteredParts.length - 1]}`
